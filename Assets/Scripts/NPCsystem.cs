7aca4b3c9b71afbf71e9d971ed6fd108c91b013a
sol <115901324+m3rcry@users.noreply.github.com>
2023-05-11 16:22:20 +0100

    scream

--- Assets/Scripts/NPCsystem.cs
+++ Assets/Scripts/NPCsystem.cs
@@ -5,49 +5,6 @@ using UnityEngine.UI;
 
 public class NPCsystem : MonoBehaviour
 {
-    public GameObject dialoguePanel;
-    public Text dialogueText;
-    public string[] dialogue;
-    private int index;
-
-    public float wordSpeed;
-    public bool playerIsClose;
-
-    void update();
-    {
-        if(input.GetKeyDown(KeyCode.E) && playerIsClose);
-        {
-            if (dialoguePanel.activeInHierarchy)
-            {
-
-            }
-        }   else
-            {
-                dialoguePanel.SetActive(true);
-            }
-    }
-    
-    public void zeroText()
-    {
-        dialogueText = "";
-        index = 0;
-        dialoguePanel.SetActive(false);
-    }
-    
-    private void OnTriggerEnter2D(Collider2D other);
-    {
-        if(other.CompareTag("player"));
-        {
-            playerIsClose = true;
-        }
-    }
-    
-    private void OnTriggerExit2D(Collider2D other);
-    {
-        if(other.CompareTag("player"));
-        {
-            playerIsClose = false;
-        }
-    }
+   
 }
 //(^._.^)
