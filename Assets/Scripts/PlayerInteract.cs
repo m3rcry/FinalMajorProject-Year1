bb36a69145ef805e39aa72823cef4b9672e8d1cd
sol <115901324+m3rcry@users.noreply.github.com>
2023-05-16 16:55:45 +0100

    AAAAAAAAAAAHHHHHHHHHHHH

---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+   private void Update() 
+   {
+       if (Input.GetKeyDown(KeyCode.E)){
+           float interactRange = 5f;
+           Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+           foreach (Collider collider in colliderArray) 
+           {
+               Debug.Log(collider);
+           }
+       }
+   }
+}
